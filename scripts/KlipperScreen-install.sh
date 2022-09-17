b8433f4f9430c7e2c3da8d0e2739df8736d3630a
Cyril <guilouz6@hotmail.com>
2022-09-17 18:55:18 +0200

    Update KlipperScreen-install.sh

--- scripts/KlipperScreen-install.sh
+++ scripts/KlipperScreen-install.sh
@@ -39,7 +39,6 @@ echo_ok ()
 install_packages()
 {
     echo_text "Update package data"
-    cp -f /home/pi/KlipperScreen/scripts/webcam.txt /home/pi/klipper_config/webcam1.txt #Changes
     sudo apt-get update
 
     echo_text "Checking for broken packages..."
