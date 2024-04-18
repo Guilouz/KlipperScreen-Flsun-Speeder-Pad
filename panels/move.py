a488f8b380c0da34b2d82fcbb4423e7e7364a0fb
Cyril <guilouz6@hotmail.com>
2024-04-18 22:28:44 +0200

    Update

--- panels/move.py
+++ panels/move.py
@@ -121,8 +121,10 @@ class Panel(ScreenPanel):
             max_z_velocity = max_velocity
 
         configurable_options = [
-            {"invert_x": {"section": "main", "name": _("Invert X"), "type": "binary", "value": "False"}},
-            {"invert_y": {"section": "main", "name": _("Invert Y"), "type": "binary", "value": "False"}},
+            {"invert_x": {"section": "main", "name": _("Invert X"), "type": "binary", "value": "False",
+                          "callback": self.reinit_panels}},
+            {"invert_y": {"section": "main", "name": _("Invert Y"), "type": "binary", "value": "False",
+                          "callback": self.reinit_panels}},
             {"invert_z": {"section": "main", "name": _("Invert Z"), "type": "binary", "value": "False"}},
             {"move_speed_xy": {
                 "section": "main", "name": _("XY Speed (mm/s)"), "type": "scale", "value": "50",
@@ -139,6 +141,11 @@ class Panel(ScreenPanel):
         for option in configurable_options:
             name = list(option)[0]
             self.options.update(self.add_option('options', self.settings, name, option[name]))
+
+    def reinit_panels(self, value):
+        logging.info(self._screen.panels)
+        self._screen.panels_reinit.append("bed_level")
+        self._screen.panels_reinit.append("bed_mesh")
 
     def process_update(self, action, data):
         if action != "notify_status_update":
